3de903b0e09fcdba593af40dee11dcb36f5a5d21
WizyWoo <72544537+WizyWoo@users.noreply.github.com>
2021-10-29 14:22:38 +0200

    Drawing Poggie

---
+++ Assets/Scripts/Regular/LevelDrawing.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelDrawing : MonoBehaviour
+{
+
+    public List<Vector2> LevelPosPoints;
+    public EdgeCollider2D LevelCollider;
+    public LineRenderer LevelBorder;
+    public string LevelParentName;
+    private int vertice;
+    private int border;
+    private GameObject levelParent;
+
+    private void Start()
+    {
+
+        LevelPosPoints = new List<Vector2>();
+        vertice = 0;
+        border = 0;
+
+        levelParent = new GameObject(LevelParentName);
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        if(Input.GetMouseButton(0))
+        {
+
+            Vector2 tempVector = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            LevelPosPoints.Add(tempVector);
+            LevelBorder.positionCount = vertice + 1;
+            LevelBorder.SetPosition(vertice, tempVector);
+
+            vertice++;
+
+        }
+
+    }
+
+    private void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+
+            LevelCollider.SetPoints(LevelPosPoints);
+            vertice = 0;
+
+            GameObject tempLevelBorder = new GameObject("LevelBorder " + border);
+            LineRenderer tempLineRenderer = tempLevelBorder.AddComponent<LineRenderer>();
+            EdgeCollider2D tempEdgeCollider = tempLevelBorder.AddComponent<EdgeCollider2D>();
+
+            tempLineRenderer.positionCount = LevelPosPoints.Count;
+            tempLineRenderer.material = LevelBorder.material;
+            tempLineRenderer.colorGradient = LevelBorder.colorGradient;
+            tempLineRenderer.widthCurve = LevelBorder.widthCurve;
+
+            Vector3[] tempV3Positions = new Vector3[LevelPosPoints.Count];
+            for (int i = 0; i < LevelPosPoints.Count; i++)
+            {
+                
+                tempV3Positions[i] = LevelPosPoints[i];
+
+            }
+            tempLineRenderer.SetPositions(tempV3Positions);
+
+            tempEdgeCollider.SetPoints(LevelPosPoints);
+
+            border++;
+
+            LevelPosPoints.Clear();
+            LevelCollider.SetPoints(new List<Vector2>());
+            LevelBorder.SetPositions(new Vector3[0]);
+
+            tempLevelBorder.transform.SetParent(levelParent.transform);
+
+        }
+
+    }
+
+}
