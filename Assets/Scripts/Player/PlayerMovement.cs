f3e408ecc25b8eb8164e01de352a54cc432fb04d
WizyWoo <kevin.hagfors@gmail.com>
2021-11-01 11:54:01 +0100

    uiug

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,30 +7,29 @@ public class PlayerMovement : MonoBehaviour
 
     //Movement
     [Range(0, 10)]
-    public float moveSpeed;
+    public float MoveSpeed;
     [Tooltip("should be between 0 and 1")]
-    public float controlPowerInAir, friction;
-    public float speedDampening;
-    public bool sprintOnOff, shouldSlide;
-    public bool MovementEnabled, NukeMovement;
+    public float ControlPowerInAir, Friction;
+    public float SpeedDampening;
+    public bool SprintOnOff, ShouldSlide;
+    public bool MovementEnabled;
     [Tooltip("should be the Player physicsMaterial 2D")]
     public PhysicsMaterial2D PM2D;
     private float yVel, jumpPower, damping;
-    [SerializeField]
     private Vector2 movementVector;
     //Jumping
-    public float BumpForce, jumpForce;
+    public float JumpHeight;
     private float playerControlPower, speedMultiplier, xMoveDir;
     private int jumpOnOff;
     private bool JumpInput;
     //GroundCheck
+    public bool IsGrounded;
     private float yGroundCheckOffset, groundCheckDist;
-    public bool isGrounded;
     private LayerMask maskPlayer;
     //References
+    public SpriteRenderer PlayerSprite;
     private CapsuleCollider2D playerCollider;
     private Rigidbody2D rb2D;
-    public SpriteRenderer PlayerSprite;
 
     void Start()
     {
@@ -56,7 +55,7 @@ public class PlayerMovement : MonoBehaviour
             JumpInput = false;
 
         RaycastHit2D hit2D;
-        shouldSlide = false;
+        ShouldSlide = false;
 
         Debug.DrawLine(transform.position + new Vector3(0, yGroundCheckOffset, 0), 0.5f * playerCollider.size.x * transform.localScale.y * Vector3.down + transform.position, Color.blue, 20);
         if (hit2D = Physics2D.CircleCast(transform.position + new Vector3(0, yGroundCheckOffset, 0), 50f * playerCollider.size.x * transform.localScale.y, new Vector2(0, -1), groundCheckDist, maskPlayer))
@@ -66,8 +65,8 @@ public class PlayerMovement : MonoBehaviour
             {
 
                 playerControlPower = 1;
-                isGrounded = true;
-                if (JumpInput && rb2D.velocity.y < jumpForce)
+                IsGrounded = true;
+                if (JumpInput && rb2D.velocity.y < JumpHeight)
                 {
 
                     jumpOnOff = 1;
@@ -75,15 +74,15 @@ public class PlayerMovement : MonoBehaviour
                 }
 
             }
-            else isGrounded = false;
+            else IsGrounded = false;
 
         }
-        else isGrounded = false;
+        else IsGrounded = false;
 
-        if (isGrounded == false)
+        if (IsGrounded == false)
         {
 
-            playerControlPower = controlPowerInAir;
+            playerControlPower = ControlPowerInAir;
             PM2D.friction = 0;
             rb2D.sharedMaterial = PM2D;
             playerCollider.sharedMaterial = PM2D;
@@ -93,13 +92,13 @@ public class PlayerMovement : MonoBehaviour
         {
 
             playerControlPower = 1f;
-            PM2D.friction = friction;
+            PM2D.friction = Friction;
             rb2D.sharedMaterial = PM2D;
             playerCollider.sharedMaterial = PM2D;
 
         }
 
-        if (sprintOnOff)
+        if (SprintOnOff)
         {
 
             if (Input.GetKey(KeyCode.LeftShift)) speedMultiplier = 1.6f;
@@ -107,9 +106,9 @@ public class PlayerMovement : MonoBehaviour
 
         }
 
-        if (rb2D.velocity.x >= (moveSpeed * speedMultiplier) && Input.GetAxis("Horizontal") > 0) xMoveDir = 0;
+        if (rb2D.velocity.x >= (MoveSpeed * speedMultiplier) && Input.GetAxis("Horizontal") > 0) xMoveDir = 0;
         else
-        if (rb2D.velocity.x <= (-moveSpeed * speedMultiplier) && Input.GetAxis("Horizontal") < 0) xMoveDir = 0;
+        if (rb2D.velocity.x <= (-MoveSpeed * speedMultiplier) && Input.GetAxis("Horizontal") < 0) xMoveDir = 0;
         else xMoveDir = Input.GetAxis("Horizontal");
 
         if(Input.GetAxisRaw("Horizontal") == -1)
@@ -117,25 +116,25 @@ public class PlayerMovement : MonoBehaviour
         else if(Input.GetAxisRaw("Horizontal") == 1)
             PlayerSprite.flipY = false;
 
-        yVel = rb2D.velocity.y - jumpForce;
+        yVel = rb2D.velocity.y - JumpHeight;
 
         movementVector = new Vector2(rb2D.velocity.x + (speedMultiplier * xMoveDir * playerControlPower), rb2D.velocity.y + (-yVel * jumpOnOff * jumpPower));
 
         if (Input.GetAxisRaw("Horizontal") == 0)
         {
 
-            if (shouldSlide == false && isGrounded)
+            if (ShouldSlide == false && IsGrounded)
             {
 
                 damping -= Time.deltaTime;
-                damping = Mathf.Clamp(damping, 0, speedDampening);
+                damping = Mathf.Clamp(damping, 0, SpeedDampening);
 
-                movementVector = new Vector2(rb2D.velocity.x * (damping / speedDampening), rb2D.velocity.y + (-yVel * jumpOnOff * jumpPower));
+                movementVector = new Vector2(rb2D.velocity.x * (damping / SpeedDampening), rb2D.velocity.y + (-yVel * jumpOnOff * jumpPower));
 
             }
 
         }
-        else damping = speedDampening;
+        else damping = SpeedDampening;
 
         jumpOnOff = 0;
 
