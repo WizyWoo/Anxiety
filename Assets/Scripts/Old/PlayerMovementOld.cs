e77904652ec1108461890f27d34a35abc6a9cfbe
WizyWoo <kevin.hagfors@gmail.com>
2021-11-01 10:50:03 +0100

    gfd

--- Assets/Scripts/Old/PlayerMovementOld.cs
+++ Assets/Scripts/Old/PlayerMovementOld.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovementOld : MonoBehaviour
 {
 
     //Movement
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovementOneLine : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
 
     //Movement
