910cad6d756b7fef941169ff16d3938624922792
WizyWoo <kevin.hagfors@gmail.com>
2021-11-15 07:34:41 +0100

    jhg

---
+++ Assets/Scripts/SavingScripts/SaveAndLoad.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class SaveAndLoad
+{
+
+    public SaveClass()
+    {
+
+
+
+    }
+    
+}
