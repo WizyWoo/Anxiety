be114ab9a65e8b82e1c759914cb8acaa4269089c
BrailleBell <Bellingmo.iver@gmail.com>
2021-11-29 12:01:05 +0100

    Brain Fight

--- Assets/Scripts/BoltShot.cs
+++ Assets/Scripts/BoltShot.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 public class BoltShot : MonoBehaviour
 {
 
- 
-    public GameObject NormalEye, RedEye;
+
+    //public GameObject Player;
     public Transform Synapse;
     private Quaternion originalRot;
    
@@ -22,39 +22,42 @@ public class BoltShot : MonoBehaviour
        
        
     }
-    
-    private void Shoot()
+    public void Update()
     {
+
+        Vector3 tempVector = playerTransform;
+        tempVector.z = 0;
+
+        tempVector.x = tempVector.x - Synapse.position.x;
+        tempVector.y = tempVector.y - Synapse.position.y;
+
+        float dif = Mathf.Atan2(tempVector.y * -1, tempVector.x * -1) * Mathf.Rad2Deg;
+        AngleRot = Quaternion.Euler(0, 0, dif - 90);
+
+    }
+    //public void Shoot()
+    //{
 
    
 
-            RaycastHit2D hit;
-            if(hit = Physics2D.Raycast(Synapse.position, playerTransform - Synapse.position, Mathf.Infinity, layerMask))
-            {
+    //        RaycastHit2D hit;
+    //        if(hit = Physics2D.Raycast(Synapse.position, playerTransform - Synapse.position, Mathf.Infinity, layerMask))
+    //        {
 
-                Debug.DrawLine(Synapse.position, hit.point, Color.green, 10f);
+    //            Debug.DrawLine(Synapse.position, hit.point, Color.green, 10f);
 
-                if(hit.collider.tag == "Player")
-                {
+    //            if(hit.collider.tag == "Player")
+    //            {
 
-                    Vector3 tempVector = playerTransform;
-                    tempVector.z = 0;
-
-                    tempVector.x = tempVector.x - Synapse.position.x;
-                    tempVector.y = tempVector.y - Synapse.position.y;
-
-                    float dif = Mathf.Atan2(tempVector.y, tempVector.x) * Mathf.Rad2Deg;
-                    AngleRot = Quaternion.Euler(0, 0, dif - 90);
-
-               
+    //            Player = hit.collider.gameObject;
                   
 
-                }
+    //            }
          
 
-            }
+    //        }
 
-        }
+    //    }
         
 
     
